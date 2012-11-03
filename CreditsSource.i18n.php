2aaff5bbca20f437fd65452e041cfb9e15130b65
raymond <raimond.spekking@gmail.com>
2012-11-03 16:46:28 +0100

    Some consistency tweaks in preparation to translate.
    Remove empty qqq messages
    Remove . at end of description message
    Add standard header to i18n file
    Add https: as default protocol to extension credits
    
    Change-Id: I34bcaf2b9a37d429e69c923e6e0cad94334a0cd6

--- CreditsSource.i18n.php
+++ CreditsSource.i18n.php
@@ -1,9 +1,15 @@
 <?php
+/**
+ * Internationalisation file for CreditsSource extension.
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ */
 
 $messages = array();
 
 $messages['en'] = array(
-	'creditssource-desc'            => 'Display source work credits in the page footer.',
+	'creditssource-desc'            => 'Display source work credits in the page footer',
 	'creditssource-source-work'     => 'This article is derived from the article $1 on $2 in its revision from $3 (UTC). There, it is published under the CC-by-sa 3.0 licence. More details and the full list of contributors can be found on the associated $5. (Wikitravel contributors are marked by the prefix $4).',
 	'creditssource-historypage'     => 'history page',
 	'creditssource-creditpage'      => 'Gives credits for sourceworks.',
@@ -11,7 +17,6 @@ $messages['en'] = array(
 
 $messages['qqq'] = array(
 	'creditssource-desc'            => '{{desc}}',
-	'creditssource-source-work'     => '',
-	'creditssource-historypage'     => '',
 	'creditssource-creditpage'      => 'Description to be displayed on the credits action page.',
 );
+
