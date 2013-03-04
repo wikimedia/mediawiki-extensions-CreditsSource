c20064d1a9b45ac876cdcda64e15454ccaf9ed32
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-03-04 09:21:28 +0100

    Tweak 'creditssource-source-work'
    Loosely based on spot by Shirayuki in
    https://translatewiki.net/wiki/Thread:Support/About_MediaWiki:Creditssource-source-work/en
    
    Change-Id: I05087b8dfce4bb4dc083486c626c808e9ff695df

--- CreditsSource.i18n.php
+++ CreditsSource.i18n.php
@@ -10,7 +10,7 @@ $messages = array();
 
 $messages['en'] = array(
 	'creditssource-desc'            => 'Display source work credits in the page footer',
-	'creditssource-source-work'     => 'This article is derived from the article $1 on $2 in its revision from $3 (UTC). There, it is published under the CC-by-sa 3.0 licence. More details and the full list of contributors can be found on the associated $5. (Wikitravel contributors are marked by the prefix $4).',
+	'creditssource-source-work'     => 'This page is derived from the page $1 on $2 in its revision from $3 (UTC). There, it is published under the CC BY-SA 3.0 license. More details and the full list of contributors can be found on the associated $5. Wikitravel contributors are marked by the prefix $4.',
 	'creditssource-historypage'     => 'history page',
 	'creditssource-creditpage'      => 'Gives credits for sourceworks.',
 );
