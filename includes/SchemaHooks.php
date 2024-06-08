c22b0fb2eb78005d1e5fba8c2645751c62e36438
Umherirrender <umherirrender_de.wp@web.de>
2024-06-08 23:34:14 +0200

    Use namespaced DatabaseUpdater class
    Change-Id: Ibc2d6f5d5545998bfa6c9b423acdd75a851d53e5

--- includes/SchemaHooks.php
+++ includes/SchemaHooks.php
@@ -2,7 +2,7 @@
 
 namespace MediaWiki\Extension\CreditSource;
 
-use DatabaseUpdater;
+use MediaWiki\Installer\DatabaseUpdater;
 use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
 
 class SchemaHooks implements LoadExtensionSchemaUpdatesHook {
